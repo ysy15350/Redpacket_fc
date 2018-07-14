ae12e698ceb4a7962a0014d96ac9df089e8ca160
ice.deng <ice.deng@qq.com>
2018-07-15 07:52:24 +0800

    用户资料

---
+++ app/src/main/java/com/ysy15350/redpacket_fc/mine/UserInfoPresenter.java
@@ -0,0 +1,31 @@
+package com.ysy15350.redpacket_fc.mine;
+
+import android.content.Context;
+
+import com.ysy15350.ysyutils.base.mvp.BasePresenter;
+
+import api.UserApi;
+import api.impl.UserApiImpl;
+
+
+public class UserInfoPresenter extends BasePresenter<UserInfoViewInterface> {
+
+    public UserInfoPresenter(Context context) {
+        super(context);
+
+    }
+
+    private UserApi userApi = new UserApiImpl();
+
+    //    public void login(){
+//        userApi.login("test", "test", new ApiCallBack() {
+//            @Override
+//            public void onSuccess(boolean isCache, Response response) {
+//                super.onSuccess(isCache, response);
+//            }
+//        });
+//    }
+//
+
+
+}
---
+++ app/src/main/java/com/ysy15350/redpacket_fc/mine/UserInfoViewInterface.java
@@ -0,0 +1,11 @@
+package com.ysy15350.redpacket_fc.mine;
+
+/**
+ * Created by yangshiyou on 2017/10/30.
+ */
+
+public interface UserInfoViewInterface {
+
+
+
+}
